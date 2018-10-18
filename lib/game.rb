cdd7d1784da0b18516678d853a484ab8d4b64ef6
Raylene <smith.raylene@gmail.com>
2018-10-18 17:00:11 +0100

    Added change_turn method and current_term variable

--- lib/game.rb
+++ lib/game.rb
@@ -1,14 +1,20 @@
 class Game
 
-  attr_reader :player1, :player2
+  attr_reader :player1, :player2, :current_turn
 
   def initialize(player1, player2)
     @player1 = player1
     @player2 = player2
+    @current_turn = player1
   end
 
   def attack(player)
     player.receive_attack
+    change_turn(player)
+  end
+
+  def change_turn(player)
+    @current_turn = player
   end
 
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -9,6 +9,10 @@ describe Game do
       expect(game.player1).to eq "Mike"
       expect(game.player2).to eq "Bob"
     end
+    it 'sets current_turn to player1' do
+      game = Game.new("Mike", "Bob")
+      expect(game.current_turn).to eq "Mike"
+    end
   end
 
   describe 'attack(player)' do
@@ -18,6 +22,17 @@ describe Game do
       game = Game.new(player1, player2)
       expect(game.player2).to receive(:receive_attack)
       game.attack(game.player2)
+    end
+  end
+
+  describe 'change_turn' do
+    it'changes the turn from player1 to player2' do
+      player1 = Player.new("Mike")
+      player2 = Player.new("Bob")
+      game = Game.new(player1, player2)
+      expect(game.player2).to receive(:receive_attack)
+      game.attack(game.player2)
+      expect(game.current_turn).to eq player2
     end
   end
 
